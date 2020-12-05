6a4b41abbe4fd4d3d542cd689236fb6c7234e80d
sg <sg@sgdeMacBook-Pro.local>
2020-12-06 01:01:47 +0800

    master

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	fmt.Println(111)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
